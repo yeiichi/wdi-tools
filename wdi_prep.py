46aca181f2a851d35fd2a468c6068265f7c1eba0
Eiichi YAMAMOTO <eiichi@Argon.local>
2019-06-10 00:18:04 +0900

    DIR structure change, etc

--- wdi_prep.py
+++ wdi_prep.py
@@ -9,8 +9,10 @@ import pandas as pd
 #Data source: World Development Indicators
 WDI_URL = "http://databank.worldbank.org/data/download/WDI_csv.zip"
 #Setup
-PROJECT_DIR = "."
-MY_DATA_PATH = os.path.join(PROJECT_DIR, "myData")
+PROJECT_DIR = os.path.join(".", "MyWDI")
+if not os.path.isdir(PROJECT_DIR):
+    os.makedirs(PROJECT_DIR)
+MY_DATA_PATH = os.path.join(PROJECT_DIR, "MyData")
 if not os.path.isdir(MY_DATA_PATH):
     os.makedirs(MY_DATA_PATH)
 ZIP_PATH = os.path.join(MY_DATA_PATH, WDI_URL.split("/")[-1])
@@ -18,6 +20,8 @@ FILE_PATH = os.path.join(MY_DATA_PATH, "WDIData.csv")
 
 #Retrieve the target
 if not os.path.isfile(ZIP_PATH):
+    print("Download started at")
+    os.system("date")
     print("Downloading... This may take a few minutes.")
     urllib.request.urlretrieve(WDI_URL, ZIP_PATH)
     print(WDI_URL.split("/")[-1], "has been downloaded to", MY_DATA_PATH)
@@ -27,11 +31,8 @@ else:
 #Unpack the downloaded zip
 if not os.path.isfile(FILE_PATH):
     shutil.unpack_archive(ZIP_PATH, MY_DATA_PATH)
-    print("Files have been unpacked into", MY_DATA_PATH)
+    print("\nFiles have been unpacked into", MY_DATA_PATH)
 else:
-    print("Files are ready at", MY_DATA_PATH)
-
-df = pd.read_csv(FILE_PATH)
-print("Country Name:\n", str(df["Country Name"].unique()))
-print("Indicator Name:\n", str(df["Indicator Name"].unique()))
-print("Years:\n", "from", df.columns[4], "to", df.columns[-2],)
+    print("\nFiles are ready at", MY_DATA_PATH)
+#List of the unpacked data
+os.system("ls -lh " + MY_DATA_PATH)
